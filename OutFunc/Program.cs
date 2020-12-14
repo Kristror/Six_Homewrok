91bfe7fa061cdee21f61ace97c1d8b6d5e8c4e79
Kristror <daniil.bezy@ya.ru>
2020-12-14 16:19:58 +0500

    Added Students

--- OutFunc/Program.cs
+++ OutFunc/Program.cs
@@ -6,31 +6,8 @@ using System.Threading.Tasks;
 
 namespace OutFunc
 {
-    class Program
+    partial class Program
     {
-        class Output
-        {
-            public delegate double Func(double x, double a);
-            public void Table(Func F, double x, double b, double a)
-            {
-                Console.WriteLine("----- X ----- Y -----");
-                while (x <= b)
-                {
-                    Console.WriteLine("| {0,8:0.000} | {1,8:0.000} |", x, F(x,a));
-                    x += 1;
-                }
-                Console.WriteLine("---------------------");
-            }
-            public double Ax(double x, double a)
-            {
-                return a * x * x;
-            }
-            public double aSin(double x, double a)
-            {
-                return a * Math.Sin(x);
-            }
-
-        }
         static void Main(string[] args)
         {
             ///
---
+++ Students/Student.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Students
+{
+    partial class Program
+    {
+        class Student
+        {
+            public string lastName;
+            public string firstName;
+            public string group;
+            public int course;
+            int age;
+
+            public Student()
+            {
+
+            }
+            public Student(string lastName, string firstName, string group, int course, int age)
+            {
+                this.lastName = lastName;
+                this.firstName = firstName;
+                this.group = group;
+                this.course = course;
+                this.age = age;
+            }
+            public List<Student> ReadStuds(string path)
+            {
+                List<Student> result = new List<Student>();
+                if (File.Exists(path))
+                {
+                    StreamReader sr = new StreamReader(path);
+
+                    while (!sr.EndOfStream)
+                    {
+                        string[] str = sr.ReadLine().Split(' ');
+                        result.Add(new Student(str[0], str[1], str[2], int.Parse(str[3]), int.Parse(str[4])));
+                    }
+                    sr.Close();
+                    return result;
+                } else return result;
+            }
+            public int CountCourse(List<Student> students, int course)
+            {
+                int count = 0;
+                foreach (Student stud in students)
+                {
+                    if (course == stud.course) count++;
+                }
+                return count;
+            }
+            /// <summary>
+            /// Сортирует массив: true - по возрастанию, true - по убыванию
+            /// </summary>
+            /// <param name="stud">массив студентов</param>
+            /// <param name="type">true - по возрастанию, true - по убыванию</param>
+            public void SortAge(ref List<Student> stud, bool type)
+            {
+                for (int i = 0; i < stud.Count - 1; i++)
+                {
+                    for (int j = i + 1; j < stud.Count; j++)
+                    {
+
+                        if (type)
+                        {
+                            if (stud[i].age > stud[j].age)
+                            {
+                                var temp = stud[i];
+                                stud[i] = stud[j];
+                                stud[j] = temp;
+                            }
+                        }
+                        else
+                        {
+                            if (stud[i].age < stud[j].age)
+                            {
+                                var temp = stud[i];
+                                stud[i] = stud[j];
+                                stud[j] = temp;
+                            }
+                        }
+                    }
+                }
+            }
+            public Dictionary<int, int> Contunt18_20 (List<Student> studs)
+            {
+                Dictionary<int, int> list = new Dictionary<int, int>();
+                foreach (var student in studs)
+                {
+                    if((student.age >= 18)&&(student.age < 20))
+                    {
+                        if (!list.ContainsKey(student.course))
+                        {
+                            list.Add(student.course, 1);
+                        }
+                        else
+                        {
+                            list[student.course]++; 
+                        }
+                    }
+                }
+                return list;
+            }
+            
+            public delegate bool check(Student stud);
+            public int Count(List<Student> studs, check func)
+            {
+                int count = 0;
+                foreach (var student in studs)
+                {
+                    if (func(student)) count++;
+                }
+                return count;
+            }
+            public string StudentToString
+            {
+                get{
+                return ($"Фамилия: {this.lastName, -15} имя: {this.firstName, -10}  факульет: {this.group, -4} курс: {this.course, -1} возраст: {this.age,-2} лет.");
+                }
+            }
+            public void Print(List<Student> students)
+            {
+                foreach(var stud in students)
+                {
+                    Console.WriteLine(stud.StudentToString);
+                }
+            }
+        }        
+    }
+}
